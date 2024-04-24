9fb67e63f3c4fba17fab542790360ec3d831e940
HyperJAK <james.jakthesamething@gmail.com>
2024-04-24 20:32:27 +0300

    Added shop page with search and features recipe

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,11 +1,16 @@
 import Image from 'next/image'
 import Logo from '@/components/shared/Logo'
 import Nav from '@/components/shared/Nav'
+import ShopSearch from '@/components/shop/ShopSearch'
+import FeaturedGadgets from '@/components/shop/FeaturedGadgets'
 
 export default function Shop() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+      <main className="relative z-30 flex h-auto flex-col justify-center gap-y-[150px]">
+        <ShopSearch />
+        <FeaturedGadgets />
+      </main>
     </>
   )
 }
